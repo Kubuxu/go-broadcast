0ed3ce906fa80123c5e9b943b32eb2309cb7d0cf
Jakub Sztandera <kubuxu@protocol.ai>
2024-01-29 20:39:52 +0100

    Initial commit
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

---
+++ broadcast.go
@@ -0,0 +1,74 @@
+package broadcast
+
+import "sync"
+
+// Channel provides a broadcast channel semantics with closing of subscribers in case of back-pressure.
+type Channel[T any] struct {
+	lk        sync.Mutex
+	listeners []chan<- T
+}
+
+// Subscribe is used to subscribe to the broadcast channel.
+// If the passed channel is full at any point, it will be dropped from subscription and closed.
+// To stop subscribing, either the closer function can be used or the channel can be abandoned.
+// Passing a channel multiple times to the Subscribe function will result in a panic.
+func (c *Channel[T]) Subscribe(ch chan<- T) (closer func()) {
+	c.lk.Lock()
+	defer c.lk.Unlock()
+	for _, exCh := range c.listeners {
+		if exCh == ch {
+			panic("channel passed multiple times to Subscribe()")
+		}
+	}
+	c.listeners = append(c.listeners, ch)
+
+	return func() {
+		c.lk.Lock()
+		defer c.lk.Unlock()
+		for i, listener := range c.listeners {
+			if listener == ch {
+				// Remove the channel from the slice without preserving the order
+				c.listeners[i] = c.listeners[len(c.listeners)-1]
+				c.listeners = c.listeners[:len(c.listeners)-1]
+				break
+			}
+		}
+		close(ch)
+	}
+}
+
+func (c *Channel[T]) Publish(val T) {
+	c.lk.Lock()
+	defer c.lk.Unlock()
+	var delinquents []chan<- T
+	for _, ch := range c.listeners {
+		select {
+		case ch <- val:
+		default:
+			delinquents = append(delinquents, ch)
+		}
+	}
+	if len(delinquents) == 0 {
+		return
+	}
+
+	// Remove delinquent channels in place
+	n := 0
+	for _, ch := range c.listeners {
+		isDelinquent := false
+		for _, dch := range delinquents {
+			if ch == dch {
+				isDelinquent = true
+				break
+			}
+		}
+		if !isDelinquent {
+			c.listeners[n] = ch
+			n++
+		}
+	}
+	for _, dch := range delinquents {
+		close(dch)
+	}
+	c.listeners = c.listeners[:n]
+}
---
+++ broadcast_test.go
@@ -0,0 +1,118 @@
+package broadcast
+
+import (
+	"runtime"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSubscribeAndPublish(t *testing.T) {
+	var ch Channel[int]
+	wg := &sync.WaitGroup{}
+	c := make(chan int, 1)
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		assert.Equal(t, <-c, 42, "Expected published value to be received")
+	}()
+
+	ch.Subscribe(c)
+	ch.Publish(42)
+	wg.Wait()
+}
+
+func TestChannelClose(t *testing.T) {
+	var ch Channel[int]
+	c := make(chan int, 1)
+
+	closer := ch.Subscribe(c)
+	closer()
+
+	_, ok := <-c
+	assert.Equal(t, false, ok, "Expected closed channel")
+}
+func TestMultipleSubscribers(t *testing.T) {
+	var ch Channel[int]
+	wg := &sync.WaitGroup{}
+
+	// Create 5 subscribers
+	for i := 0; i < 5; i++ {
+		c := make(chan int, 1)
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			assert.Equal(t, <-c, 42, "Expected published value to be received")
+		}()
+
+		ch.Subscribe(c)
+	}
+
+	runtime.Gosched()
+	ch.Publish(42)
+
+	// Wait for all subscribers to receive the value
+	wg.Wait()
+}
+
+func TestMultipleSubscription(t *testing.T) {
+	var ch Channel[int]
+	c := make(chan int, 1)
+
+	ch.Subscribe(c)
+	assert.Panics(t, func() { ch.Subscribe(c) }, "Expected panic on multiple subscription")
+}
+
+func TestBackPressure(t *testing.T) {
+	var ch Channel[int]
+	c := make(chan int, 1)
+
+	ch.Subscribe(c)
+	ch.Publish(42)
+	ch.Publish(42)
+
+	assert.Equal(t, 42, <-c)
+	_, ok := <-c
+	assert.Equal(t, false, ok, "Expected channel to be closed due to back pressure")
+}
+
+func TestCloser(t *testing.T) {
+	var ch Channel[int]
+	c := make(chan int, 1)
+
+	closer := ch.Subscribe(c)
+
+	ch.Publish(42)
+
+	val, ok := <-c
+	assert.True(t, ok, "Expected channel to be open")
+	assert.Equal(t, 42, val, "Expected published value to be received")
+
+	closer()
+
+	_, ok = <-c
+	assert.False(t, ok, "Expected channel to be closed")
+}
+
+func TestBackPressureWithMultipleReceivers(t *testing.T) {
+	var ch Channel[int]
+	c1 := make(chan int, 1)
+	c2 := make(chan int)
+
+	ch.Subscribe(c1)
+	ch.Subscribe(c2)
+
+	ch.Publish(42)
+
+	// Check that the value was received on c1
+	val, ok := <-c1
+	assert.True(t, ok, "Expected channel to be open")
+	assert.Equal(t, 42, val, "Expected published value to be received")
+
+	// Check that c2 was closed due to backpressure
+	_, ok = <-c2
+	assert.False(t, ok, "Expected channel to be closed due to backpressure")
+}
